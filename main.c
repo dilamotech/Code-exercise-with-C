77e44a80cda5ee89c964d380aa6a082e813ea01e
dilamotech <ef.dilamo@gmail.com>
2026-09-10 21:40:27 +0300

    some more exercise

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main () {
     char grade = '\0' ;
     char arrayOfChar[] = "array of character";
     char name[50]= "";
-    bool isLord = true;
+    bool isStudent = false;
     int d = 5;
     int r = 5;
     int s = 0;
@@ -25,9 +25,42 @@ int main () {
       //q = ceil(q);
       //q = sin(q);
       //q = cos(q);
-      q = tan(q);
+      //q = tan(q);
     //printf("%d", z);
-    printf("%f", q);
+    //printf("%f", q);
+    /* printf("Enter your age: ");
+    scanf("%d", &age);
+    if(age >= 65)
+    {
+    printf("You are a senior\n");
+     }
+    else if(age >= 18){
+   printf("You are an adult\n");
+    }
+    else if(age < 0 ){
+     printf("You have not been born yet\n");
+    }
+    else if(age == 0){
+    printf("You are a newborn\n"); 
+    }
+    else {
+      printf("You are a child\n");
+    }
+    if(isStudent){
+	    printf("You are a student\n");
+       }
+    else {
+       printf("You are Not a student\n");}*/
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+    if(strlen(name) == 0){
+    printf("You did not enter your name\n");
+    }
+    else{
+       printf("Hello %s\n", name);
+    }
+
     d++;
     d--;
     
@@ -61,6 +94,6 @@ int main () {
     printf("this is char %c\n",grade);
     printf("this is an array of character %s\n",arrayOfChar);
     printf("It's really good\n");
-    printf("Jesus is lord%d",isLord);
+    printf("Dilamo is a student%d\n",isStudent);
     return 0;
 }

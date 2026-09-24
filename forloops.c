b3743f49290fbcab43398c91fb97b92b0c9a3bd7
dilamotech <ef.dilamo@gmail.com>
2026-09-24 06:53:21 +0300

    forloop

---
+++ forloops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+int main(){
+    // for loop = repeat some code a limited # of times
+    // for(Initialization; Condition; Update)
+
+
+	for(int i = 10; i > 0; i--){
+		Sleep(1000);
+      printf("%d\n",i);
+	}
+printf("Happy New Year!\n");
+
+ return 0;
+}
